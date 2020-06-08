72ebee0e05fb1ca386a33ac1e92cf52528315911
Ana Ordonez <anaordonez@comcast.net>
2020-06-08 13:22:19 -0700

    add comments and formatting

--- rav_get.py
+++ rav_get.py
@@ -10,6 +10,7 @@ import csv
 def ravelry_search(rav_query, rav_session):
     # rav_query: dictionary of query items
     # rav_auth: list of ravelry name and password
+    # returns the request response object
     
     rav_url = "https://api.ravelry.com/patterns/search.json"
     rav_params = rav_query
@@ -21,7 +22,7 @@ def ravelry_search(rav_query, rav_session):
 def ravelry_patterns(rav_ids, rav_session):
     # rav_ids: space delimited string of ids
     # rav_auth: list of ravelry name and password
-    # returns dictionary of patterns
+    # returns the request response object
     
     rav_url = "https://api.ravelry.com/patterns.json"
     rav_params = {'ids':rav_ids}
@@ -34,6 +35,7 @@ def ravelry_get_ids(rav_result):
     # Returns the id string that can be used to query patterns
     # input: rav_result: output from ravelry_search
     # output: id_list: formatted string of ids
+    
     rav_json = rav_result.json()
     
     id_list = ''
@@ -41,11 +43,13 @@ def ravelry_get_ids(rav_result):
     for key in range(len(rav_json['patterns'])):
         id_list += (str(rav_json['patterns'][key]['id']) + ' ')
         
+    # remove the last white space
     return id_list[:-1]
 
 def ravelry_load_auth(auth_file):
     # loads your ravelry api authentication info from csv
-    # and creates a session
+    # and creates and returns a session object
+    
     rav_session = requests.Session()
     
     with open(auth_file) as f:
@@ -53,4 +57,5 @@ def ravelry_load_auth(auth_file):
         rav_auth = list(reader)
         
     rav_session.auth = (rav_auth[0][0], rav_auth[0][1])
+    
     return rav_session
