de53b67dac599606989f3b970ac7b333ce39bebc
Ana Ordonez <anaordonez@comcast.net>
2020-06-12 10:37:14 -0700

    add designer function

--- rav_get.py
+++ rav_get.py
@@ -35,6 +35,18 @@ def ravelry_project_search(rav_query, rav_session):
     
     return rav_request
 
+def ravelry_designer(designer_id, rav_session):
+    # designer_id: string, the designer id number
+    # rav_session: requests session object
+    # returns the request response object
+    
+    rav_url = "https://api.ravelry.com/designers/"+str(designer_id)+".json"
+
+    rav_request = rav_session.get(url = rav_url)
+    
+    rav_request.raise_for_status()
+    
+    return rav_request
 
 def ravelry_patterns(rav_result, rav_session):
     # rav_ids: space delimited string of ids
@@ -57,7 +69,6 @@ def ravelry_get_ids(rav_result):
     # id_list: formatted string of ids
     
     rav_json = rav_result.json()
-    
     id_list = ''
     
     for key in range(len(rav_json['patterns'])):
